30aacb2c9e7c3d712f1651092affb358d309c8b2
devicepac <bardichedevice@gmail.com>
2023-08-25 17:06:12 +0900

    Feat: 글쓰기 작업 중

--- src/pages/NoticeWrite.js
+++ src/pages/NoticeWrite.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
-import { StudentListWrap } from "../styles/StudentListStyle";
+import React, { useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router";
-import { NoticeWhite } from "../styles/NoticeStyle";
+import {
+  NoticeButton,
+  NoticeWhiteNoitce,
+  NoticeWrap,
+} from "../styles/NoticeStyle";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const NoticeWrite = () => {
   const [title, setTitle] = useState("");
@@ -9,12 +14,75 @@ const NoticeWrite = () => {
   const [isImportant, setIsImportant] = useState(false);
   const navigate = useNavigate();
 
+  // ReactQull 태그 reference 저장
+  const quillRef = useRef(null);
+
+  // 툴바 설정하기
+  // 화면이 갱신될때마다 새로 정의할 필요가 없음.
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          [{ font: [] }],
+          [{ align: [] }],
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ list: "ordered" }, { list: "bullet" }, "link"],
+          [
+            {
+              color: [
+                "#000000",
+                "#e60000",
+                "#ff9900",
+                "#ffff00",
+                "#008a00",
+                "#0066cc",
+                "#9933ff",
+                "#ffffff",
+                "#facccc",
+                "#ffebcc",
+                "#ffffcc",
+                "#cce8cc",
+                "#cce0f5",
+                "#ebd6ff",
+                "#bbbbbb",
+                "#f06666",
+                "#ffc266",
+                "#ffff66",
+                "#66b966",
+                "#66a3e0",
+                "#c285ff",
+                "#888888",
+                "#a10000",
+                "#b26b00",
+                "#b2b200",
+                "#006100",
+                "#0047b2",
+                "#6b24b2",
+                "#444444",
+                "#5c0000",
+                "#663d00",
+                "#666600",
+                "#003700",
+                "#002966",
+                "#3d1466",
+                "custom-color",
+              ],
+            },
+            { background: [] },
+          ],
+        ],
+      },
+    }),
+    [],
+  );
+
   const handleTitleChange = event => {
     setTitle(event.target.value);
   };
 
   const handleContentChange = event => {
-    setContent(event.target.value);
+    setContent(event);
   };
   const handleCheckboxChange = event => {
     setIsImportant(event.target.checked);
@@ -31,10 +99,13 @@ const NoticeWrite = () => {
     setIsImportant(false);
     navigate(-1);
   };
+  const handleCencle = () => {
+    navigate(-1);
+  };
 
   return (
-    <StudentListWrap>
-      <h1>게시판 글쓰기</h1>
+    <NoticeWrap>
+      <h3>게시판 글쓰기</h3>
       <label>
         중요 공지 시 체크
         <input
@@ -44,7 +115,7 @@ const NoticeWrite = () => {
         />
       </label>
       <form onSubmit={handleSubmit}>
-        <NoticeWhite>
+        <div>
           <label>제목</label>
           <input
             type="text"
@@ -52,17 +123,19 @@ const NoticeWrite = () => {
             onChange={handleTitleChange}
             required
           />
-          <div className="form-group">
-            <textarea
-              className="form-control summernote"
-              rows="5"
-              id="content"
-            ></textarea>
-          </div>
-          <button type="submit">글쓰기</button>
-        </NoticeWhite>
+        </div>
+        <NoticeWhiteNoitce>
+          <ReactQuill
+            ref={quillRef}
+            value={content}
+            onChange={handleContentChange}
+            modules={modules}
+          />
+        </NoticeWhiteNoitce>
+        <NoticeButton type="submit">글쓰기</NoticeButton>
       </form>
-    </StudentListWrap>
+      <NoticeButton onClick={handleCencle}>취소</NoticeButton>
+    </NoticeWrap>
   );
 };
 
