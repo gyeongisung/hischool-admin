d54d13ccb8c3667c65435123aa8cba43396eac13
gyeongisung <gisung.gyeon@gmail.com>
2023-09-05 16:23:39 +0900

    feat: TypeScript 환경설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -6,13 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 // import { Provider } from "react-redux";
 import { CookiesProvider } from "react-cookie";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") ?? document.createElement("div"),
+);
 root.render(
   <CookiesProvider>
     {/* <Provider> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     {/* </Provider> */}
   </CookiesProvider>,
 );
