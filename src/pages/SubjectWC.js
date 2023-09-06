13fee16733b5212f5d7ec29de38bb0fabfd1c491
devicepac <bardichedevice@gmail.com>
2023-09-06 13:12:57 +0900

    Feat : 과목 수정작업중

--- src/pages/SubjectWC.js
+++ src/pages/SubjectWC.js
@@ -13,7 +13,6 @@ import TSubjectUpdate from "../components/subject/TSubjectUpdate";
 const SubjectWC = () => {
   const { gradeId } = useParams();
   const [gradeData, setGradeData] = useState([]);
-  const [listDelete, setListDelete] = useState([]); // 초기값을 빈 배열로 설정
   console.log(gradeData);
   useEffect(() => {
     const fetchData = async () => {
@@ -40,8 +39,6 @@ const SubjectWC = () => {
             <TSubjectUpdate
               key={index}
               item={item}
-              setListDelete={setListDelete} // setListDelete 함수 전달
-              listDelete={listDelete} // listDelete 상태 전달
               gradeData={gradeData}
               setGradeData={setGradeData}
             />
