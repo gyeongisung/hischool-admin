2cd8eaaef0309aea8d808232a27101f3d32073e6
gyeongisung <gisung.gyeon@gmail.com>
2023-09-25 15:22:09 +0900

    feat: 배포 준비

--- src/api/signUpAxios.js
+++ src/api/signUpAxios.js
@@ -3,9 +3,13 @@ import { client } from "./client";
 
 export const postSignUp = async formData => {
   try {
-    const res = await axios.post("/api/sign-up", formData, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
+    const res = await axios.post(
+      `${process.env.REACT_APP_API_URL}/api/sign-up`,
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      },
+    );
   } catch (err) {
     console.error(err);
   }
